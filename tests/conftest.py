9435afcfbb92bf1833003bc455da05949f4863e6
Dmitriy <osmolovskiy@yandex.ru>
2025-02-06 23:47:21 +0200

    Дополнено 4

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,8 @@ from data import PAGE_URL
 @pytest.fixture(scope="session")
 def page_driver():
     options = webdriver.ChromeOptions()
-    # options.add_argument('window-size=1920,1080')
-    options.add_argument('--headless=new')
+    options.add_argument('window-size=1920,1080')
+    options.add_argument('--headless')
     # options.add_argument('no-sandbox')
     # options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=options)
